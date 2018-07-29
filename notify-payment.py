3ed87bdef996dddaacc9b5f660b72f0fe19fc740
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-07-29 18:51:28 +0800

    add basic procedure doc

---
+++ notify-payment.py
@@ -0,0 +1,10 @@
+# Input: 
+# mandatory - invoice number, type (water (w) or electricity(e), pdf) 
+# optional - email address
+
+# Procedure:
+# 1. find pdf
+# 2. Make email text (with invoice number)
+# 3. Add pdf as attachment
+# 4. confirmation 
+# 5. Send email
